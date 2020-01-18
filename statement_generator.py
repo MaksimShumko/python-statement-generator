5d157361cc6115d0ed472273d8d81e8d4e27793a
Maksim Shumko <mszumko@gmail.com>
2020-01-18 17:14:05 +0100

    done

--- statement_generator.py
+++ statement_generator.py
@@ -1,7 +1,85 @@
-class OperationGenerator:
+import random
+
+class Balance:
+    def __init__(self, value):
+        self.value = value
     
-    def __init__(self, file):
+    def plus(self, value):
+        self.value += value
+        
+    def minus(self, value):
+        self.value -= value
+        
+    def get(self):
+        return self.value
+
+class OperationParameter:
+    def __init__(self, parameter):
+        self.parameter = parameter
+    
+    def printParameter(self, value):
+        return "\n\t\t<" + self.parameter + ">" + value + "" + "<\\" + self.parameter + ">"
+        
+class ExecDate(OperationParameter):
+    def __init__(self):
+        OperationParameter.__init__(self, "exec-date")
+        
+    def printParameter(self, i):
+        month = int((i + 2) / 30) + 1
+        day = ((i + 2) % 30) + 1
+        value = "2019-0" + str(month) + "-" + str(day)
+        return OperationParameter.printParameter(self, value)
+    
+class OrderDate(OperationParameter):
+    def __init__(self):
+        OperationParameter.__init__(self, "order-date")
+        
+    def printParameter(self, i):
+        month = int(i / 30) + 1
+        day = (i % 30) + 1
+        value = "2019-0" + str(month) + "-" + str(day)
+        return OperationParameter.printParameter(self, value)
+    
+class Type(OperationParameter):
+    def __init__(self):
+        OperationParameter.__init__(self, "type")
+        
+    def printParameter(self, i):
+        return OperationParameter.printParameter(self, "Platnosc karta")
+    
+class Description(OperationParameter):
+    def __init__(self):
+        OperationParameter.__init__(self, "description")
+        
+    def printParameter(self, i, price):
+        month = int(i / 30) + 1
+        day = (i % 30) + 1
+        title = "Tytul: 09016606177698710044149"
+        localization = "Lokalizacja: Kraj: POLSKA Miasto: Poznan Adres: Poznan    91237"
+        date = "Data i czas operacji: 2019-0" + str(month) + "-" + str(day)
+        amount = "Oryginalna kwota operacji: " + str(price) + " PLN"
+        number = "Numer karty: 265497******4678"
+        value = title + "\n" + localization + "\n" + date + "\n" + amount + "\n" + number
+        return OperationParameter.printParameter(self, value)
+    
+class Amount(OperationParameter):
+    def __init__(self):
+        OperationParameter.__init__(self, "amount")
+        
+    def printParameter(self, i, price):
+        return "\n\t\t<" + self.parameter + " curr='PLN'>-" + str(price) + "</" + self.parameter + ">"
+    
+class EndingBalance(OperationParameter):
+    def __init__(self):
+        OperationParameter.__init__(self, "ending-balance")
+        
+    def printParameter(self, i, balance):
+        return "\n\t\t<" + self.parameter + " curr='PLN'>+" + str(round(balance.get(), 2)) + "</" + self.parameter + ">"
+
+class OperationGenerator:    
+    def __init__(self, file, balance):
         self.file = file
+        self.balance = balance
         
     def printStart(self):
         self.write("\n")
@@ -9,11 +87,14 @@ class OperationGenerator:
         self.write("<operation>")
 
     def printOperation(self, i):
-        self.write("\n")
-        self.write("\t\t")
-        month = int(i / 30) + 1
-        day = (i % 30) + 1
-        self.write("<exec-date>2019-0" + str(month) + "-" + str(day) + "</exec-date>")
+        price = round(random.uniform(0.01, 99.99), 2)
+        self.balance.minus(price)
+        self.write(ExecDate().printParameter(i))
+        self.write(OrderDate().printParameter(i))
+        self.write(Type().printParameter(i))
+        self.write(Description().printParameter(i, price))
+        self.write(Amount().printParameter(i, price))
+        self.write(EndingBalance().printParameter(i, self.balance))
         
     def printEnd(self):
         self.write("\n")
@@ -24,7 +105,6 @@ class OperationGenerator:
         self.file.write(text)
 
 class Generator:
-    
     def __init__(self):
         self.file = open("history_20200110_416816.xml", "w+")
         
@@ -38,8 +118,9 @@ class Generator:
         self.write("<operations>")
         
     def printOperations(self):
+        balance = Balance(10000)
         for i in range(100):
-            operation = OperationGenerator(self.file)
+            operation = OperationGenerator(self.file, balance)
             operation.printStart()
             operation.printOperation(i)
             operation.printEnd()
